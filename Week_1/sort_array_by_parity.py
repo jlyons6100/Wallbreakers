c33504e63be01f4fe863ecf9f7ea18534da1a481
James Lyons <james@MacBook-Air-369.local>
2019-06-21 15:44:09 -0400

    Finished sort_array_by_parity

---
+++ Week_1/sort_array_by_parity.py
@@ -0,0 +1,6 @@
+# Sort Array By Parity:
+# Returns array of all the even elements of A followed by all the odd elements of A
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return [x for x in A if x % 2 == 0] + [x for x in A if x % 2 != 0]
