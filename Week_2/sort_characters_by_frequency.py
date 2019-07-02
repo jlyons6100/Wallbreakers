2be7162c3eb9fbcd90545770e71c939d9adad295
James Lyons <jlyons6100@gmail.com>
2019-07-02 19:59:57 -0400

    Update sort_characters_by_frequency.py

--- Week_2/sort_characters_by_frequency.py
+++ Week_2/sort_characters_by_frequency.py
@@ -3,14 +3,6 @@
 from collections import defaultdict
 class Solution:
     def frequencySort(self, s: str) -> str:
-        d = defaultdict(int)
-        for ltr in s:
-            d[ltr] += 1
-        # print(d.items())
-        sorted_x = sorted(d.items(), key=lambda kv: kv[1])
-        sorted_x.reverse()
-        strr = ""
-        for tup in sorted_x:
-            for i in range(tup[1]):
-                strr += tup[0]
-        return (strr)
+        c = Counter(s)
+        ret = c.most_common(len(c))
+        return "".join([tup[0]*tup[1] for tup in ret])
