b1a0198b9db6985c199d45fe5da04ef88ec9d75a
James Lyons <jlyons6100@gmail.com>
2019-09-01 16:22:22 -0400

    Update groups_of_special_equivalent_strings.py

--- Week_2/groups_of_special_equivalent_strings.py
+++ Week_2/groups_of_special_equivalent_strings.py
@@ -1,16 +1,18 @@
 # Groups of Special Equivalent Strings: Given an array A of strings.
 # Two strings are special equivalent if after any number of moves S == T
 
-from collections import Counter, defaultdict
+from collections import defaultdict
 class Solution:
     def numSpecialEquivGroups(self, A: List[str]) -> int:
         groups = defaultdict(list)
-        for word in A:
-            even = Counter([word[ind] for ind in range(len(word)) if ind % 2 == 0])
-            odd = Counter([word[ind] for ind in range(len(word)) if ind % 2 == 1])
-            f_e = frozenset(even.items())
-            f_o = frozenset(odd.items())
-            tup = (f_e, f_o)
-            groups[tup].append(word)
+        for string in A:
+            even = ''
+            odd = ''
+            for i, ltr in enumerate(string):
+                if i % 2 == 0:
+                    even += ltr
+                else:
+                    odd += ltr 
+            groups[("".join(sorted(even)),"".join(sorted(odd)))].append(string)
         return len(groups)
            
